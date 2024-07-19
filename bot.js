997249f33c892ac021247978d4816a808980dc75
dreamsandtarla <derstime23@gmail.com>
2024-07-20 00:06:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 /*
+,
 
   ________.__                        _____.___.___________
  /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
@@ -10,7 +11,8 @@
 ╔════════════════════════════════════════════════════════════════════════╗
 ║                                                                        ║
 ║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
+║  ## Feel free to utilize any portion of the code                   
+║
 ║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
 ║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
 ║                                                                        ║
